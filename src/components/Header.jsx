a8c71919f0fcd4cfff667d02afcac6115f6bf5dc
MM-27-dev <monikamuniraju27@gmail.com>
2025-06-09 04:31:59 +0530

    AI Chat Funactinlity Implemented using gemini AI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
+    <div className=" px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
       <img className="w-30" src={LOGO} alt="Logo Image" />
       {user && (
         <div className="flex">
