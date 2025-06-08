799f68c04eb4191819aafb3c0c67852497c3a10f
Monika <monikamuniraju27@gmail.com>
2025-06-09 05:10:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import LoginForm from "./components/LoginForm";
 import AuthProvider from "../src/HOC/AuthProvider";
 import ProtectedRoute from "../src/HOC/ProtectedRoute";
 import PublicRoute from "../src/HOC/PublicRoute";
-import appStore from "./utils/appstore";
+import appStore from "./utils/appStore";
 
 
 function App() {
